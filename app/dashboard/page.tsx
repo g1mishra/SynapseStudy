08b5ea50fcdd02db42927b9f97cbbf1815746dcb
Jeevan Kumar <g1mishra.dev@gmail.com>
2023-05-24 02:12:24 +0530

    Swr used to cache the auth data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import Profile from "./Profile";
 export default function Dashboard() {
   return (
     <main className="hero min-h-[calc(100vh-80px)] bg-base-200 py-4">
-      <div className="hero-content container justify-evenly flex-col sm:flex-row">
+      <div className="hero-content container justify-evenly flex-col">
         <h1 className="text-5xl font-bold">Dashboard</h1>
         <Profile />
       </div>
--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,26 +1,50 @@
 "use client";
 
+// import { getCurrentUser } from "@/lib/auth";
+// import { useRouter } from "next/navigation";
+// import { useEffect, useState } from "react";
+
+// export function useAuth() {
+//   const [currentUser, setCurrentUser] = useState<any>(null);
+//   const [loading, setLoading] = useState(true);
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     // Fetch current user data on component mount
+//     getCurrentUser()
+//       .then((userData: any) => {
+//         // Store the current user data in state
+//         setCurrentUser(userData);
+//         setLoading(false);
+//       })
+//       .catch((error: any) => {
+//         console.error("Failed to fetch current user:", error);
+//         // Set error state and redirect to login or handle authentication error
+//         setLoading(false);
+//         router.push("/login");
+//       });
+//   }, []);
+
+//   return { currentUser, loading };
+// }
+
 import { getCurrentUser } from "@/lib/auth";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export function useAuth() {
-  const [currentUser, setCurrentUser] = useState<any>(null);
   const router = useRouter();
 
-  useEffect(() => {
-    // Fetch current user data on component mount
-    getCurrentUser()
-      .then((userData: any) => {
-        // Store the current user data in state
-        setCurrentUser(userData);
-      })
-      .catch((error: any) => {
-        console.error("Failed to fetch current user:", error);
-        // Redirect to login or handle authentication error
-        router.push("/login");
-      });
-  }, []);
+  const { data: currentUser, error } = useSWR("/api/auth", getCurrentUser, {
+    onError: (error) => {
+      console.error("Failed to fetch current user:", error);
+      router.push("/login");
+    },
+    revalidateOnFocus: false, // Disable revalidation on focus to prevent unnecessary fetches
+    shouldRetryOnError: false, // Disable retrying on error to prevent infinite loop
+  });
 
-  return currentUser;
+  const loading = !currentUser && !error;
+
+  return { currentUser, loading, error };
 }
