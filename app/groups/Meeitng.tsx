9787f013f59e4547f7b5b60ddaef68544d348197
Jeevan Kumar <g1mishra.dev@gmail.com>
2023-05-27 18:55:38 +0530

    Jitsi SDK added

---
+++ app/groups/Meeitng.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import Loading from "@/components/Loading";
+import { JitsiMeeting } from "@jitsi/react-sdk";
+import { IJitsiMeetExternalApi } from "@jitsi/react-sdk/lib/types";
+import React, { useRef, useState } from "react";
+
+interface LogItem {
+  message: string;
+}
+
+const Meeting: React.FC = () => {
+  const apiRef = useRef<IJitsiMeetExternalApi | null>(null);
+  const [logItems, updateLog] = useState<LogItem[]>([]);
+  const [showNew, toggleShowNew] = useState(false);
+  const [knockingParticipants, updateKnockingParticipants] = useState<any[]>([]);
+
+  const printEventOutput = (payload: any) => {
+    updateLog((items) => [...items, { message: JSON.stringify(payload) }]);
+  };
+
+  const handleAudioStatusChange = (payload: any, feature: string) => {
+    if (payload.muted) {
+      updateLog((items) => [...items, { message: `${feature} off` }]);
+    } else {
+      updateLog((items) => [...items, { message: `${feature} on` }]);
+    }
+  };
+
+  const handleChatUpdates = (payload: any) => {
+    if (payload.isOpen || !payload.unreadCount) {
+      return;
+    }
+    apiRef.current?.executeCommand("toggleChat");
+    updateLog((items) => [
+      ...items,
+      { message: `you have ${payload.unreadCount} unread messages` },
+    ]);
+  };
+
+  const handleKnockingParticipant = (payload: any) => {
+    updateLog((items) => [...items, { message: JSON.stringify(payload) }]);
+    updateKnockingParticipants((participants) => [...participants, payload?.participant]);
+  };
+
+  const resolveKnockingParticipants = (condition: (participant: any) => boolean) => {
+    knockingParticipants.forEach((participant) => {
+      apiRef.current?.executeCommand(
+        "answerKnockingParticipant",
+        participant?.id,
+        condition(participant)
+      );
+      updateKnockingParticipants((participants) =>
+        participants.filter((item) => item.id === participant.id)
+      );
+    });
+  };
+
+  const handleJitsiIFrameRef1 = (iframeRef: HTMLDivElement | null) => {
+    if (iframeRef) {
+      iframeRef.style.border = "10px solid #3d3d3d";
+      iframeRef.style.background = "#3d3d3d";
+      iframeRef.style.height = "calc(100vh - 80px)";
+      iframeRef.style.width = "100%";
+      iframeRef.style.marginBottom = "20px";
+    }
+  };
+
+  const handleApiReady = (apiObj: IJitsiMeetExternalApi) => {
+    apiRef.current = apiObj;
+    apiRef.current.on("knockingParticipant", handleKnockingParticipant);
+    apiRef.current.on("audioMuteStatusChanged", (payload: any) =>
+      handleAudioStatusChange(payload, "audio")
+    );
+    apiRef.current.on("videoMuteStatusChanged", (payload: any) =>
+      handleAudioStatusChange(payload, "video")
+    );
+    apiRef.current.on("raiseHandUpdated", printEventOutput);
+    apiRef.current.on("titleViewChanged", printEventOutput);
+    apiRef.current.on("chatUpdated", handleChatUpdates);
+    apiRef.current.on("knockingParticipant", handleKnockingParticipant);
+  };
+
+  const handleReadyToClose = () => {
+    /* eslint-disable-next-line no-alert */
+    alert("Ready to close...");
+  };
+
+  const generateRoomName = (): string => `JitsiMeetRoomNo${Math.random() * 100}-${Date.now()}`;
+
+  const renderSpinner = () => (
+    <div className="fixed inset-0 flex items-center justify-center min-h-screen">
+      <Loading className="relative" />
+    </div>
+  );
+
+  return (
+    <>
+      <JitsiMeeting
+        roomName={generateRoomName()}
+        spinner={renderSpinner}
+        configOverwrite={{
+          //   subject: "lalalala",
+          hideConferenceSubject: false,
+          enableClosePage: true,
+        }}
+        onApiReady={handleApiReady}
+        onReadyToClose={handleReadyToClose}
+        getIFrameRef={handleJitsiIFrameRef1}
+      />
+    </>
+  );
+};
+
+export default Meeting;
--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,6 +1,13 @@
-export default function Loading() {
+import { cn } from "@/utils/utils";
+
+export default function Loading({ className = "" }) {
   return (
-    <div className="fixed inset-0 bg-base-200 flex items-center justify-center min-h-screen">
+    <div
+      className={cn(
+        "fixed inset-0 bg-base-200 flex items-center justify-center min-h-screen ",
+        className
+      )}
+    >
       <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
     </div>
   );
