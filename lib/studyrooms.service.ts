d8efa400e41ffe3b355f69e27b6c06881579a2fc
Jeevan Kumar <g1mishra.dev@gmail.com>
2023-05-31 01:57:45 +0530

    crud service added for study groups

--- lib/studyrooms.service.ts
+++ lib/studyrooms.service.ts
@@ -1,72 +1,129 @@
+import { StudyRoomI, StudyRoomModel, UserLinksI, UserLinksModel } from "@/types/study-room";
 import { Server } from "@/utils/config";
+import { ID, Models, Permission, Query, Role } from "appwrite";
 import appwriteSDKProvider from "./appwrite.client";
-import { Models, Query } from "appwrite";
-import { StudyRoomModel } from "@/types/study-room";
 
 const studyRoomsCollectionID = "646dcbc3c72be67e1267";
 const userLinksCollectionID = "646dcbd31368a4a94979";
 
-export async function getAllStudyRooms(): Promise<
-  Models.DocumentList<StudyRoomModel>
-> {
-  return await appwriteSDKProvider.database.listDocuments(
-    Server.dbId,
-    studyRoomsCollectionID
-  );
+export async function getAllStudyRooms(): Promise<Models.DocumentList<StudyRoomModel>> {
+  return await appwriteSDKProvider.database.listDocuments(Server.dbId, studyRoomsCollectionID);
 }
 
-export async function getUserJoinedStudyRooms([_url, userId]: [
-  string,
-  string
-]): Promise<Models.DocumentList<StudyRoomModel>> {
+export async function getUserJoinedStudyRooms([_url, userId]: [string, string]): Promise<Models.DocumentList<StudyRoomModel>> {
   const userJoinedStudyRooms = await appwriteSDKProvider.database.listDocuments(
     Server.dbId,
     userLinksCollectionID,
     [Query.equal("user_id", userId)]
   );
 
-  const studyRoomIds = userJoinedStudyRooms.documents.map(
-    (room) => room.study_room_id
-  );
+  const studyRoomIds = userJoinedStudyRooms.documents.map((room) => room.study_room_id);
 
   if (studyRoomIds.length === 0) {
     return { documents: [], total: 0 };
   }
 
   const userJoinedStudyRoomDetails: Models.DocumentList<StudyRoomModel> =
-    await appwriteSDKProvider.database.listDocuments(
-      Server.dbId,
-      studyRoomsCollectionID,
-      [Query.equal("$id", studyRoomIds)]
-    );
+    await appwriteSDKProvider.database.listDocuments(Server.dbId, studyRoomsCollectionID, [
+      Query.equal("$id", studyRoomIds),
+    ]);
 
   return userJoinedStudyRoomDetails;
 }
 
-export async function getUnjoinedStudyRooms([_url, userId]: [
-  string,
-  string
-]): Promise<Models.DocumentList<StudyRoomModel>> {
+export async function getUnjoinedStudyRooms([_url, userId]: [string, string]): Promise<Models.DocumentList<StudyRoomModel>> {
   const userJoinedStudyRooms = await appwriteSDKProvider.database.listDocuments(
     Server.dbId,
     userLinksCollectionID,
     [Query.equal("user_id", userId)]
   );
 
-  const studyRoomIds = userJoinedStudyRooms.documents.map(
-    (room) => room.study_room_id
-  );
+  const studyRoomIds = userJoinedStudyRooms.documents.map((room) => room.study_room_id);
 
   if (studyRoomIds.length === 0) {
     return getAllStudyRooms();
   }
 
   const unjoinedStudyRoomDetails: Models.DocumentList<StudyRoomModel> =
-    await appwriteSDKProvider.database.listDocuments(
-      Server.dbId,
-      studyRoomsCollectionID,
-      [Query.notEqual("$id", studyRoomIds)]
-    );
+    await appwriteSDKProvider.database.listDocuments(Server.dbId, studyRoomsCollectionID, [
+      Query.notEqual("$id", studyRoomIds),
+    ]);
 
   return unjoinedStudyRoomDetails;
+}
+
+export async function getStudyRoom([_url, studyRoomId]: [string, string]): Promise<StudyRoomModel> {
+  return await appwriteSDKProvider.database.getDocument(Server.dbId, studyRoomsCollectionID, studyRoomId);
+}
+
+
+
+export async function joinStudyRoom([_url, payload]: [string, UserLinksI]): Promise<UserLinksModel | any> {
+  
+  const { study_room_id: studyRoomId, user_id: userId, role = "user" } = payload;
+
+  const userLink = await appwriteSDKProvider.database.createDocument(Server.dbId, userLinksCollectionID, ID.unique(), {
+    study_room_id: studyRoomId,
+    user_id: userId,
+    role: role,
+  });
+
+  return userLink;
+}
+
+export async function leaveStudyRoom([_url, studyRoomId, userId]: [string, string, string]): Promise<any> {
+  const userLink = await appwriteSDKProvider.database.listDocuments(Server.dbId, userLinksCollectionID, [
+    Query.equal("study_room_id", studyRoomId),
+    Query.equal("user_id", userId),
+  ]);
+
+  if (userLink.documents.length === 0) {
+    throw new Error("User not found in study room");
+  }
+
+  const userLinkId = userLink.documents[0].$id;
+
+  return await appwriteSDKProvider.database.deleteDocument(Server.dbId, userLinksCollectionID, userLinkId);
+}
+
+export async function createStudyRoom([_url, userId, data]: [string, string, StudyRoomI]): Promise<StudyRoomModel | any> {
+  const { name, description, image_url, subject } = data;
+  const studyRoom = await appwriteSDKProvider.database.createDocument(Server.dbId, studyRoomsCollectionID, ID.unique(), {
+    name,
+    description,
+    image_url: image_url,
+    subject,
+  },
+  [
+    Permission.delete(Role.user(userId)),
+    Permission.update(Role.user(userId)),
+  ]
+  );
+
+  await joinStudyRoom(["", { study_room_id: studyRoom.$id, user_id: userId, role: "owner" }]);
+
+  return studyRoom;
+}
+
+export async function updateStudyRoom([_url, data]: [string, StudyRoomModel]): Promise<StudyRoomModel | any> {
+  const { name, description, image_url, subject,$id :studyRoomId  } = data;
+  const studyRoom = await appwriteSDKProvider.database.updateDocument(Server.dbId, studyRoomsCollectionID, studyRoomId, {
+    name,
+    description,
+    image_url,
+    subject,
+  });
+
+  return studyRoom;
+}
+
+export async function deleteStudyRoom([_url, studyRoomId]: [string, string]): Promise<any> {
+  const userLinks = await appwriteSDKProvider.database.listDocuments(Server.dbId, userLinksCollectionID, [
+    Query.equal("study_room_id", studyRoomId),
+  ]);
+
+  // need to check deleteAll at once
+  userLinks.documents.forEach(async (userLink) => await appwriteSDKProvider.database.deleteDocument(Server.dbId, userLinksCollectionID, userLink.$id));
+
+  return await appwriteSDKProvider.database.deleteDocument(Server.dbId, studyRoomsCollectionID, studyRoomId);
 }
