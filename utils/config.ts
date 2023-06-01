c9b504165ac7ee46f59695b1f689df719fb63aa7
Jeevan Kumar <g1mishra.dev@gmail.com>
2023-06-01 12:46:33 +0530

    env updated

--- utils/config.ts
+++ utils/config.ts
@@ -4,8 +4,8 @@ export const Server = {
   dbId: process.env.NEXT_PUBLIC_APPWRITE_DB_ID ?? "",
 
   // collections
-  roomsCollectionId: process.env.APPWRITE_ROOMS_COLLECTION_ID ?? "",
-  userLinksCollectionId: process.env.APPWRITE_USER_LINKS_COLLECTION_ID ?? "",
+  roomsCollectionId: process.env.NEXT_PUBLIC_ROOMS_COLLECTION_ID ?? "",
+  userLinksCollectionId: process.env.NEXT_PUBLIC_USERS_COLLECTION_ID ?? "",
 
   // secret keys
   documentReadKey: process.env.APPWRITE_DOCUMENT_READ_KEY ?? "",
