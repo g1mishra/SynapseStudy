485b404fd9cf78155383b160ed9024d0f3799de2
Vikas0786 <vthakur412000@gmail.com>
2023-06-10 00:11:04 +0530

    build test fix

--- app/(protected)/study-rooms/page.tsx
+++ app/(protected)/study-rooms/page.tsx
@@ -1,4 +1,4 @@
-import StudyRooms from "../../components/StudyRooms";
+import StudyRooms from "@/components/StudyRooms";
 
 const StudyRoom = () => {
   return (
@@ -6,8 +6,9 @@ const StudyRoom = () => {
       <div className="flex flex-col items-center text-center py-8 overflow-hidden">
         <h1 className="text-xl font-bold p-4">Welcome</h1>
         <p className="max-w-screen-md">
-          This is a study room. You can create a study room and invite your friends to join you. You
-          can also join a study room created by your friends.
+          This is a study room. You can create a study room and invite your
+          friends to join you. You can also join a study room created by your
+          friends.
         </p>
         <div className="mt-8 max-w-screen-md w-full">
           <StudyRooms />
