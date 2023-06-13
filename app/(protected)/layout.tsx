be85890d77df00e4655304a45527f3799336b74e
Jeevan Kumar <g1mishra.dev@gmail.com>
2023-06-13 19:44:14 +0530

    minor fix

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -49,7 +49,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
 
   return (
     <main className="flex h-screen max-h-screen overflow-hidden">
-      <MenuBar logout={logout} className={cn("hidden md:block")} />
+      <MenuBar logout={logout} className={cn("hidden md:flex")} />
       <div className="w-full bg-black-primary overflow-y-auto [&>div]:pt-0 md:[&>div]:pt-8">
         <Header className="flex md:hidden my-6 px-8 pb-4 max-w-full" currentUser={currentUser} />
         {children}
