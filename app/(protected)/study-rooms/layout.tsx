f7f768229a61dfe0dedc5d441dc4d39f8829fef9
Jeevan Kumar <g1mishra.dev@gmail.com>
2023-06-14 03:12:36 +0530

    Mobile Responsive

--- app/(protected)/study-rooms/layout.tsx
+++ app/(protected)/study-rooms/layout.tsx
@@ -1,21 +1,30 @@
-import StudyRoomSidebar from "./StudyRoomSidebar";
+"use client";
+
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { cn } from "@/utils/utils";
+import dynamic from "next/dynamic";
+
+const StudyRoomSidebar = dynamic(() => import("./StudyRoomSidebar"));
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const matches = useMediaQuery("(min-width: 768px)");
   return (
     <div className="flex flex-col !pt-0">
       <div className="flex flex-row overflow-hidden">
-        <div
-          className={cn(
-            "hidden test123 md:flex flex-col shrink-0 sm:w-full min-w-[350px] w-full md:w-1/6 h-screen overflow-hidden bg-black-tertiary"
-          )}
-        >
-          <StudyRoomSidebar />
-        </div>
+        {matches && (
+          <div
+            className={cn(
+              "hidden md:flex flex-col shrink-0 sm:w-full min-w-[350px] w-full md:w-1/6 h-screen overflow-hidden bg-black-tertiary"
+            )}
+          >
+            <StudyRoomSidebar />
+          </div>
+        )}
+
         {children}
       </div>
     </div>
