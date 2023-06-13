362f2221b6f12cb48a58fec8ff7ac5aed0bf7564
Jeevan Kumar <g1mishra.dev@gmail.com>
2023-06-13 23:36:02 +0530

    finally build error fixed

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -8,17 +8,15 @@ import {
   WhiteboardIcon,
 } from "@/components/Icons";
 import { useAuth } from "@/hooks/useAuth";
+import { cn } from "@/utils/utils";
+import dynamic from "next/dynamic";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 import Header from "./Header";
 import MenuBar from "./MenuBar";
-import { cn } from "@/utils/utils";
-import { useState } from "react";
-import dynamic from "next/dynamic";
-import { useParams, usePathname } from "next/navigation";
 // import StudyRoomSidebar from "./study-rooms/StudyRoomSidebar";
 
-const StudyRoomSidebar = dynamic(
-  () => import("./study-rooms/StudyRoomSidebar")
-);
+const StudyRoomSidebar = dynamic(() => import("./study-rooms/StudyRoomSidebar"));
 
 const routes = [
   {
@@ -51,17 +49,12 @@ const routes = [
   },
 ];
 
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const { currentUser, logout } = useAuth();
   const [openMenu, setOpenMenu] = useState(false);
 
-  const shouldShowStudyRoomSidebar =
-    pathname.includes("/study-rooms") && openMenu;
+  const shouldShowStudyRoomSidebar = pathname.includes("/study-rooms") && openMenu;
 
   return (
     <main className="flex h-screen max-h-screen overflow-hidden">
@@ -76,12 +69,9 @@ export default function DashboardLayout({
       ) : null}
 
       <div
-        className={cn(
-          "w-full bg-black-primary overflow-y-auto [&>div]:pt-0 md:[&>div]:pt-8",
-          {
-            "w-[50px]": shouldShowStudyRoomSidebar,
-          }
-        )}
+        className={cn("w-full bg-black-primary overflow-y-auto [&>div]:pt-0 md:[&>div]:pt-8", {
+          "w-[50px]": shouldShowStudyRoomSidebar,
+        })}
         onClick={() => {
           shouldShowStudyRoomSidebar && setOpenMenu(false);
         }}
@@ -89,7 +79,6 @@ export default function DashboardLayout({
         <Header
           className="flex md:hidden my-6 px-8 pb-4 max-w-full"
           currentUser={currentUser}
-          openMenu={openMenu}
           setOpenMenu={setOpenMenu}
         />
 
