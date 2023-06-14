8e16876e1e64747bb344813e8f1b39db23ab128b
Vikas0786 <vthakur412000@gmail.com>
2023-06-14 22:38:59 +0530

    landing page initialization

--- app/(general)/about/page.tsx
+++ app/(general)/about/page.tsx
@@ -1,20 +1,29 @@
+import { AboutSectionCreative } from "@/components/Icons";
+
 const AboutPage = () => {
   return (
-    <main className="min-h-[calc(100vh-80px)] bg-base-200">
+    <main className="min-h-[calc(100vh-80px)] bg-black-primary text-white">
       <section className="py-8 container mx-auto">
-        <div className="container mx-auto px-4">
-          <h1 className="text-4xl font-bold mb-4">About SynapseStudy</h1>
+        <div className="container mx-auto px-4 text-center mt-32">
+          {/* <h1 className="text-4xl font-bold mb-4">About SynapseStudy</h1> */}
           <p className="text-lg mb-4">
-            SynapseStudy is a platform designed to enhance collaboration and knowledge sharing among
-            students. It provides a range of features to facilitate virtual study rooms, real-time
-            messaging, document sharing, virtual whiteboard, video conferencing, and notifications.
+            SynapseStudy is a platform designed to enhance collaboration and
+            knowledge sharing among students. It provides a range of features to
+            facilitate virtual study rooms, real-time messaging, document
+            sharing, virtual whiteboard, video conferencing, and notifications.
           </p>
           <p className="text-lg mb-4">
-            Our goal is to create a seamless and intuitive study experience, allowing students to
-            connect with their peers, collaborate on subjects, share resources, and engage in
-            interactive study sessions.
+            Our goal is to create a seamless and intuitive study experience,
+            allowing students to connect with their peers, collaborate on
+            subjects, share resources, and engage in interactive study sessions.
           </p>
-          <p className="text-lg">Join SynapseStudy today and revolutionize the way you study!</p>
+          <p className="text-lg">
+            Join SynapseStudy today and revolutionize the way you study!
+          </p>
+          <div className="scale-75 ml-52">
+            {" "}
+            <AboutSectionCreative />
+          </div>
         </div>
       </section>
     </main>
--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -1,4 +1,7 @@
 import CTAButton from "@/components/CTAButton";
+import AboutPage from "./about/page";
+import Image from "next/image";
+import { bucketFilePath } from "@/utils/utils";
 // async function getData() {
 //   try {
 //     const user = await getCurrentUser();
@@ -11,14 +14,28 @@ import CTAButton from "@/components/CTAButton";
 export default async function Page() {
   // const data = await getData();
   return (
-    <main className="hero min-h-[calc(100vh-80px)] bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-screen-md">
-          <h1 className="text-5xl font-bold">SynapseStudy</h1>
-          <p className="py-6">Collaborative online learning platform for virtual study groups</p>
-          <CTAButton />
+    <>
+      <main className="hero h-[calc(70vh)] bg-[#ffffff]">
+        <div className="hero-content text-center">
+          <div className="max-w-screen-md">
+            <h1 className="text-5xl font-bold text-[#F9762E]">Synapse Study</h1>
+            <p className="py-6">
+              Collaborative online learning platform for virtual study groups
+            </p>
+            {/* <CTAButton /> */}
+          </div>
         </div>
+      </main>
+      <div className="relative">
+        <Image
+          src={bucketFilePath("assets", "6489efbe17d765d66735")}
+          width={"600"}
+          height={"600"}
+          alt="fancy-banner"
+          className="absolute top-0 left-1/2 translate-x-1/2 -translate-y-1/2 inset-y-0"
+        />
       </div>
-    </main>
+      <AboutPage />
+    </>
   );
 }
